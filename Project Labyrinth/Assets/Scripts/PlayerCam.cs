c916809aafe97fc05f81f4c80da52e3291a5e4d1
Ren Demeis-Ortiz <ldemeisortiz@gmail.com>
2022-04-13 23:22:33 -0400

    Updated camera panning on left mouse click and drag. Added player rotation.

--- Project Labyrinth/Assets/Scripts/PlayerCam.cs
+++ Project Labyrinth/Assets/Scripts/PlayerCam.cs
@@ -11,21 +11,27 @@ public class PlayerCam : MonoBehaviour
     private float xRotation = 0.0f;
     private float yRotation = 0.0f;
     private Camera cam;
- 
+
     void Start()
     {
+        // Source: https://forum.unity.com/threads/get-a-camera-by-tag.14186/
+        // cam = GameObject.FindGameObjectWithTag("Player").GetComponent<Camera>() as Camera;
         cam = Camera.main;
     }
- 
+
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
- 
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
- 
-        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+            float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+
+            yRotation += mouseX;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+            cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+
+        }
     }
 }
--- Project Labyrinth/Assets/Scripts/PlayerMovement.cs
+++ Project Labyrinth/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector3 moveValues;
     CharacterController characterController;
-    public float MovementSpeed =1;
+    public float MovementSpeed = 5f;
+    public float RotationSpeed = 5f;
     public float Gravity = 9.8f;
-    private float velocity = 0;
  
+    // Source: https://docs.unity3d.com/ScriptReference/Transform.InverseTransformDirection.html
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -16,20 +18,21 @@ public class PlayerMovement : MonoBehaviour
  
     void Update()
     {
-        // player movement - forward, backward, left, right
-        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
-        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
-        characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
- 
-        // Gravity
-        if(characterController.isGrounded)
-        {
-            velocity = 0;
-        }
-        else
-        {
-            velocity -= Gravity * Time.deltaTime;
-            characterController.Move(new Vector3(0, velocity, 0));
-        }
+        // Player movement - forward, backward and rotate left, right
+        // Sources: https://www.youtube.com/embed/e5g1nJcjz-M
+        //          https://forum.unity.com/threads/character-controller-move-local-vs-global-problem.107863/
+        float horizontal = Input.GetAxis("Horizontal") * RotationSpeed;
+        // Rotate Left and Right
+        transform.Rotate(0, horizontal * Time.deltaTime, 0);
+        Vector3 vertical = Vector3.zero;
+        vertical.z = Input.GetAxis("Vertical") * MovementSpeed;
+
+
+
+        // Forward Backward
+        // Changes Direction to Local Direction to Maintain Perspective
+        moveValues = transform.TransformDirection(vertical);
+        characterController.Move(moveValues * Time.deltaTime);
+
     }
 }

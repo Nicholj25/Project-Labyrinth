5e66d2230dcb2f8b7dbec32e8c2d2eab49895547
mjanitor164 <janitorm@oregonstate.edu>
2022-04-22 15:21:21 -0400

    Added Room M, some changes to tutorial room

--- Project Labyrinth/Assets/Scripts/PlayerMovement.cs
+++ Project Labyrinth/Assets/Scripts/PlayerMovement.cs
@@ -2,45 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Player movement - forward, backward and rotate left, right
-// Sources: https://www.youtube.com/embed/e5g1nJcjz-M
-//          https://forum.unity.com/threads/character-controller-move-local-vs-global-problem.107863/
-//          https://stackoverflow.com/questions/39797696/gameobject-prefabs-floating-away
-//          https://www.youtube.com/watch?v=ixM2W2tPn6c
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 zMovement;
-    Vector3 rotationMovement;
     Vector3 moveValues;
+    CharacterController characterController;
     public float MovementSpeed = 5f;
     public float RotationSpeed = 5f;
-    private float horizontal;
-    Rigidbody rb;
+    public float Gravity = 9.8f;
  
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        zMovement = Vector3.zero;
-        rotationMovement = Vector3.zero;
+        characterController = GetComponent<CharacterController>();
     }
-
+ 
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal") * RotationSpeed;
-        zMovement.z = Input.GetAxis("Vertical") * MovementSpeed;
-    }
-    void FixedUpdate()
-    {
+        // Player movement - forward, backward and rotate left, right
+        // Sources: https://www.youtube.com/embed/e5g1nJcjz-M
+        //          https://forum.unity.com/threads/character-controller-move-local-vs-global-problem.107863/
+        float horizontal = Input.GetAxis("Horizontal") * RotationSpeed;
 
         // Rotate Left and Right
-        rotationMovement.y = horizontal;
-        Quaternion deltaRotation = Quaternion.Euler(rotationMovement * Time.fixedDeltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotation);
+        transform.Rotate(0, horizontal * Time.deltaTime, 0);
 
         // Forward Backward
         // Changes Direction to Local Direction to Maintain Perspective
-        moveValues = transform.TransformDirection(zMovement);
-        rb.MovePosition(transform.position + moveValues * Time.deltaTime);
+        Vector3 vertical = Vector3.zero;
+        vertical.z = Input.GetAxis("Vertical") * MovementSpeed;
+        moveValues = transform.TransformDirection(vertical);
+        characterController.Move(moveValues * Time.deltaTime);
 
     }
 }

35b3b6525f7fde40badebd8b71c7d291b283a042
Ren Demeis-Ortiz <ldemeisortiz@gmail.com>
2022-05-24 16:09:24 -0700

    Cleaned up commented code from reappearing item fixes

--- Project Labyrinth/Assets/Scripts/Inventory/ReappearItemAction.cs
+++ Project Labyrinth/Assets/Scripts/Inventory/ReappearItemAction.cs
@@ -18,24 +18,7 @@ public class ReappearItemAction : ItemInteraction
     public float yMax = 2f;
     public float zMin = 0.4f;
     public float zMax = 13.75f;
-    /*
-    private bool holdingItem;
-    // Forward movement force.
-    float moveForce = 1.0f;
-
-    // Torque for left/right rotation.
-    float rotateTorque = 1.0f;
-
-    // Desired hovering height.
-    float hoverHeight = 4.0f;
-
-    // The force applied per unit of distance below the desired height.
-    float hoverForce = 5.0f;
-
-    // The amount that the lifting force is reduced per unit of upward speed.
-    // This damping tends to stop the object from bouncing after passing over
-    // something.
-    float hoverDamp = 0.5f;*/
+    
     protected override void Awake()
     { 
         base.Awake();
@@ -56,42 +39,38 @@ public class ReappearItemAction : ItemInteraction
     private void OnCollisionEnter(Collision collision)
     {
         rb.velocity = Vector3.zero;
-        if (triggerObject == collision.gameObject)
+        if (triggerObject)
         {
-            InteractionComplete?.Invoke();
-            //rb.isKinematic = true;
+            if (triggerObject == collision.gameObject)
+            {
+                InteractionComplete?.Invoke();
+            }
         }
-
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (triggerObject == collision.gameObject)
+        if (triggerObject)
         {
-            Debug.Log("collision exit");
-            ItemRemoved?.Invoke();
-            //rb.isKinematic = true;
+            if (triggerObject == collision.gameObject)
+            {
+                ItemRemoved?.Invoke();
+            }
         }
     }
 
     protected override void Update()
     {
-        if (Input.GetMouseButtonDown(1) && !inventoryScreen.activeSelf)// && isInRoom())
+        if (Input.GetMouseButtonDown(1) && !inventoryScreen.activeSelf)
         {
             cam = cameraHandler.GetCurrentCamera();
             Vector3 mousePosition = Input.mousePosition;
             Inventory.RemoveItem(inventoryItem);
             Inventory.UnequipItem();
-            //holdingItem = false;
             rb.MovePosition(Vector3.Lerp(rb.position, cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)), Time.deltaTime * 5));
             rb.drag = startDrag;
             rb.mass = startMass;
             rb.freezeRotation = false;
-
-            //rb.isKinematic = false;
-            //rb.MovePosition(Vector3.Lerp(rb.position, cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)), Time.deltaTime * 5));
-
-            //rb.MovePosition(cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)));
         }
     }
     // Update is called once per frame
@@ -105,88 +84,13 @@ public class ReappearItemAction : ItemInteraction
             Vector3 mousePosition = Input.mousePosition;
             mousePosition = cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition));
             mousePosition = getInRoomPosition(mousePosition);
-            /*if (!holdingItem)
-            {
-                rb.MovePosition(Vector3.Lerp(rb.position, cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)), Time.deltaTime * 5));
-                holdingItem = true;
-            }
-            if (Input.GetMouseButtonDown(1) && !inventoryScreen.activeSelf && isInRoom())
-            {
-                Inventory.RemoveItem(inventoryItem);
-                Inventory.UnequipItem();
-                //holdingItem = false;
-                rb.MovePosition(Vector3.Lerp(rb.position, cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)), Time.deltaTime * 5));
-                rb.drag = 0;
-
-                //rb.isKinematic = false;
-                //rb.MovePosition(Vector3.Lerp(rb.position, cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)), Time.deltaTime * 5));
-
-                //rb.MovePosition(cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)));
-            }
-            else
-            {*/
-            //rb.isKinematic = true;
-
-            //rb.MovePosition(Vector3.Lerp(rb.position, cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)), Time.deltaTime * 5));
             rb.MovePosition(Vector3.Lerp(rb.position, mousePosition, Time.deltaTime * 5));
             rb.drag = 45;
             rb.mass = 0;
-            
-                /*hoverHeight = cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)).y;
-
-                // Push the object based on arrow key input.
-                rb.AddForce(Input.GetAxis("Vertical") * moveForce * transform.forward);
-                rb.AddForce(Input.GetAxis("Horizontal") * moveForce * transform.right);
-
-                RaycastHit hit;
-                Ray downRay = new Ray(transform.position, -Vector3.up);
-
-                // Cast a ray straight downwards.
-                if (Physics.Raycast(downRay, out hit))
-                {
-                    // The "error" in height is the difference between the desired height
-                    // and the height measured by the raycast distance.
-                    float hoverError = hoverHeight - hit.distance;
-
-                    // Only apply a lifting force if the object is too low (ie, let
-                    // gravity pull it downward if it is too high).
-                    if (hoverError > 0)
-                    {
-                        // Subtract the damping from the lifting force and apply it to
-                        // the rigidbody.
-                        float upwardSpeed = rb.velocity.y;
-                        float lift = hoverError * hoverForce - upwardSpeed * hoverDamp;
-                        rb.AddForce(lift * Vector3.up);
-                    }
-                }*/
-                //rb.MovePosition(cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zPosition)));
-
-            //}
-
         }
 
 
     }
-
-    /*private bool isInRoom()
-    {
-        bool inX = this.gameObject.transform.position.x >  -3.6 && this.gameObject.transform.position.x < 7.75;
-        bool inY = this.gameObject.transform.position.y > 0.01 && this.gameObject.transform.position.y < 2;
-        bool inZ = this.gameObject.transform.position.z > 0.3 && this.gameObject.transform.position.z < 13.75;
-
-        if (inX && inY && inZ)
-        {
-            Debug.Log("true isInRoom");
-            Debug.Log(this.gameObject.transform.position);
-            return true;
-        }
-        else
-        {
-            Debug.Log("false isInRoom");
-            Debug.Log(this.gameObject.transform.position);
-            return false;
-        }
-    }*/
 
     /// <summary>
     /// Checks that it is within room boundaries on x axis

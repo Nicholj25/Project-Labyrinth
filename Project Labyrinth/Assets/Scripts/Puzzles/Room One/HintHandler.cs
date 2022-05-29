9c19a51b7b7f8468613ca9266e3d5db6d54dd537
mjanitor164 <janitorm@oregonstate.edu>
2022-05-29 17:30:45 -0400

    Fixed sprites, delayed hint

--- Project Labyrinth/Assets/Scripts/Puzzles/Room One/HintHandler.cs
+++ Project Labyrinth/Assets/Scripts/Puzzles/Room One/HintHandler.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Threading.Tasks;
+using System;
 
 public class HintHandler : Puzzle
 {
@@ -13,31 +15,37 @@ public class HintHandler : Puzzle
     private bool PowerIsOn;
     private bool FileCabinetKeyFound;
     private bool FinalKeyObtained;
+    private bool PowerFlickeredOut;
 
    
     // Start is called before the first frame update 
-    void Start()
+    async void Start()
     {
         Power.InteractionComplete.AddListener(() => { PowerIsOn = true; });
         FileCabinetKey.InteractionComplete.AddListener(() => { FileCabinetKeyFound = true; });
         FinalKey.InteractionComplete.AddListener(() => { FinalKeyObtained = true; });
 
         // Start with no progress
+        PowerFlickeredOut = false;
         PowerIsOn = false;
         FileCabinetKeyFound = false;
         FinalKeyObtained = false;
+
     }
 
-    private void Update()
+    async private void Update()
     {
-        
+        await WaitOneSecondAsync(21);
+        PowerFlickeredOut = true;
     }  
 
     public override string GetCurrentHint()
     {
         string hintText = "";
 
-        if (!PowerIsOn)
+        if (!PowerFlickeredOut)
+            hintText = "The lights are buzzing. Is this an issue?";
+        else if (!PowerIsOn)
             hintText = "The power is out. There must be fuses lying around somewhere.";
         else if (!FileCabinetKeyFound)
             hintText = "Sometimes opening cabinets wildly is helpful.";
@@ -45,6 +53,12 @@ public class HintHandler : Puzzle
             hintText = "The boss always likes things in order and on time.";
 
         return hintText;
+    }
+
+    // Add a short delay
+    public async Task WaitOneSecondAsync(double seconds)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(seconds));
     }
 
 }

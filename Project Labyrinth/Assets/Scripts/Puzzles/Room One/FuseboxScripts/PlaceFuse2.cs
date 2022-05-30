416a94901b48250b09e21e8442645d65263362f2
Ren Demeis-Ortiz <ldemeisortiz@gmail.com>
2022-05-30 13:19:39 -0400

    Added variables to fix fuse and power on issues in room one

--- Project Labyrinth/Assets/Scripts/Puzzles/Room One/FuseboxScripts/PlaceFuse2.cs
+++ Project Labyrinth/Assets/Scripts/Puzzles/Room One/FuseboxScripts/PlaceFuse2.cs
@@ -9,6 +9,8 @@ public class PlaceFuse2 : ItemInteraction
     /// </summary>
     private Animator placeFuse;
     public bool fuseAdded;
+    public GameObject fuse;
+    public InventoryItem interaction;
 
     protected override void ItemUsageAction() 
     {
@@ -25,6 +27,8 @@ public class PlaceFuse2 : ItemInteraction
         fuseAdded = true;
 
         // Remove fuse from inventory
+        interaction.enabled = false;
+        fuse.tag = "Untagged";
         Inventory.RemoveItem(Inventory.CurrentItem);
         Inventory.UnequipItem();
 
